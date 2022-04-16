ded6b7dc972b1ba9802272ef96060229269f3108
mohamed  baharawy <bahrawy@MacBook-Air-mohamed.local>
2022-04-17 00:05:23 +0200

    uploading

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,69 @@
+import { getShowProductCode } from './../state/products.reducer';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { Subscription } from 'rxjs';
+
+import { Product } from '../product';
+import { ProductService } from '../product.service';
+import { State } from '../state/products.reducer';
+
+import * as ProductActions from './../state/product.actions';
+
+@Component({
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit, OnDestroy {
+  pageTitle = 'Products';
+  errorMessage: string;
+
+  displayCode: boolean;
+
+  products: Product[];
+
+  // Used to highlight the selected product in the list
+  selectedProduct: Product | null;
+  sub: Subscription;
+
+  constructor(
+    private productService: ProductService,
+    private store: Store<State>
+  ) {}
+
+  ngOnInit(): void {
+    this.sub = this.productService.selectedProductChanges$.subscribe(
+      (currentProduct) => (this.selectedProduct = currentProduct)
+    );
+
+    this.productService.getProducts().subscribe({
+      next: (products: Product[]) => (this.products = products),
+      error: (err) => (this.errorMessage = err),
+    });
+
+    // TODO: Unscbscribe
+    this.store
+      .select(getShowProductCode)
+      .subscribe(
+        (getShowProductCode) => (this.displayCode = getShowProductCode)
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  checkChanged(): void {
+    // this.displayCode = !this.displayCode;
+    this.store.dispatch(ProductActions.toggleProductCode());
+  }
+
+  newProduct(): void {
+    this.productService.changeSelectedProduct(this.productService.newProduct());
+  }
+
+  productSelected(product: Product): void {
+    this.productService.changeSelectedProduct(product);
+  }
+}
